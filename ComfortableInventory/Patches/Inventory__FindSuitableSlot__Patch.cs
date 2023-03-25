06807af90c6bbaba9a870c72b5a6a757bbe53e89
kohanis <kohanis@ya.ru>
2023-03-25 20:07:41 +0300

    Prefer inventory functional fix. Bump version to 1.2.2

--- ComfortableInventory/Patches/Inventory__FindSuitableSlot__Patch.cs
+++ ComfortableInventory/Patches/Inventory__FindSuitableSlot__Patch.cs
@@ -24,8 +24,20 @@ namespace kohanis.ComfortableInventory.Patches
                  (stackableItem.settings_buildable.Placeable || subCategory == "Batteries")))
                 return true;
 
-            __result = (Slot)Reflected.PlayerInventory__FindSuitableSlot__Invoker(self,
-                new object[] { self.hotslotCount, self.allSlots.Count, stackableItem });
+
+            if (stackableItem.settings_Inventory.Stackable)
+                __result = (Slot)Reflected.PlayerInventory__FindSuitableSlot__Invoker(self,
+                    new object[] { 0, self.hotslotCount, stackableItem });
+
+            if (__result == null || __result.IsEmpty)
+            {
+                var slot = (Slot)Reflected.PlayerInventory__FindSuitableSlot__Invoker(self,
+                    new object[] { self.hotslotCount, self.allSlots.Count, stackableItem });
+
+                if (slot != null)
+                    __result = slot;
+            }
+
             return false;
         }
     }
